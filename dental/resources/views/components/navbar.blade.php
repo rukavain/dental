5f2c5bb7996548cd1d85605b44ea1b7e38f9e1c4
rukavain1~ <magtoto599@gmail.com>
2024-05-27 23:19:28 +0800

    added base design for homepage

--- dental/resources/views/components/navbar.blade.php
+++ dental/resources/views/components/navbar.blade.php
@@ -8,9 +8,24 @@
 </head>
 
 <body>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <div class="flex p-4 justify-between align-center shadow-lg">
+        <div class="flex gap-4 text-md justify-center items-center">
+            <div>
+                <img class="h-14" src="{{ asset('images/logo.png') }}" alt="">
+            </div>
+            <h1 class="hover:font-bold transition-all cursor-pointer">Insurance</h1>
+            <h1 class="hover:font-bold transition-all cursor-pointer">Locations</h1>
+            <h1 class="hover:font-bold transition-all cursor-pointer">Services</h1>
+            <h1 class="hover:font-bold transition-all cursor-pointer">Orthodontics</h1>
+            <h1 class="hover:font-bold transition-all cursor-pointer">Emergency</h1>
+        </div>
+        <div class="flex gap-6 justify-center items-center">
+            <h1 class="font-bold">Login</h1>
+            <h1
+                class="py-2 px-4 rounded-md text-white font-semibold bg-green-600 hover:bg-green-700 transition-all cursor-pointer">
+                BOOK NOW</h1>
+        </div>
+    </div>
 </body>
 
 </html>
--- dental/resources/views/homepage.blade.php
+++ dental/resources/views/homepage.blade.php
@@ -7,10 +7,28 @@
     @vite('resources/css/app.css')
 </head>
 
-<body>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+<body class="bg-slate-100">
+    @include('components.navbar')
+    <section class="flex justify-evenly mx-4 items-center my-8 ">
+        <div class="bg-white w-lvw flex flex-col py-8 px-12 rounded-lg shadow-lg gap-6 h-min">
+            <h1 class="text-7xl font-semibold">Look forward to the dentist</h1>
+            <h1 class="text-lg">Top-rated clinicians</h1>
+
+            <div class="w-[50%] flex flex-col justify-center items-start gap-4">
+                <a class="max-w-sm text-center mt-4" href="">
+                    <h1
+                        class="bg-green-600 rounded-md py-2 px-4 font-bold text-white hover:bg-green-700 transition-all">
+                        BOOK NOW</h1>
+                </a>
+                <h1 class="text-sm">
+                    OR CALL: <span class="font-bold">212-2345-5918</span>
+                </h1>
+            </div>
+        </div>
+        <div class=" max-w-[50vw] flex justify-center items-center">
+            <img class="h-max rounded-lg" src="{{ asset('images/hero-image.jpg') }}" alt="">
+        </div>
+    </section>
 </body>
 
 </html>
